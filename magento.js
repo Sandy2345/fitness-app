c96799cdb258b1dc3ebac0c1b7bcfccde47bcaec
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-21 12:39:37 +0530

    Update magento.js

--- magento.js
+++ magento.js
@@ -32,7 +32,31 @@ var getAuthTokenService = (callback) =>{
   });
 };
 
+var getGpsFromZipService = (zipcode, callback) =>{
+  console.log(zipcode);
+  console.log('getGpsFromZip API hit');
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=AIzaSyBvrztXIMHaa-fWqtaXrpvyQ66nEH6ulzo`,
+    method: 'GET',
+    rejectUnauthorized: false,
+    json: true
+    }, (error, response, body) => {
+    if(error){
+      callback('There was an error connecting to the server');
+    }
+    else if(response.statusCode == 401){
+      callback('Unable to get the result');
+    }
+    else if(response.statusCode == 200){
+      console.log('get coordinates API hit:', response.statusCode);
+      callback (undefined, {
+        sLat : body.results[0].geometry.location.lat,
+        sLng : body.results[0].geometry.location.lng
+      });
+    }
+  });
 
+};
 var getDeviceTokenService = (token, deviceID, callback) => {
         console.log('getDeviceTokenService API hit');
         request({
@@ -128,6 +152,7 @@ function isEmpty(obj) {
 }
 
 module.exports = {
+    getGpsFromZipService,
     getAuthTokenService,
     getDeviceTokenService,
     sendPushNotificationService
