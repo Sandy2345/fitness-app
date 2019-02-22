8e2312d3394b96c7f367fe3a299df26299d9923e
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-22 09:49:12 +0530

    Update sfcc-apis.js

--- sfcc-apis.js
+++ sfcc-apis.js
@@ -381,7 +381,7 @@ var getupdatedweather = (city,applicationid, callback) => {
           else if(response.statusCode == 200){
            // console.log("sandeep.... check weather :",body.weather.description);
             callback(undefined, {
-              code:very cool
+              code: 'very cool'
               });
             }
           else {
