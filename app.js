42656455f19dabaaedf8b777bf06401faba50cb0
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-24 19:20:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const sfcc= require('./sfcc-apis.js');
 const sfmc= require('./sfmc.js');
 //const magento=require('./magento.js')
-magento-api.js
 const magento= require('./magento-api.js');
 const mailer= require('./mailer.js');
 const nodemailer= require('nodemailer');
