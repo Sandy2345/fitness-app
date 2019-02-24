0fd834bb0a08ca7aa37d60015ff583e7424709fb
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-25 00:52:34 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -71,7 +71,7 @@ console.log('Create order api');
         });
     }
     else if(response.statusCode == 200){
-      console.log('createorderService API hit:', body.items[0].amount_refunded)
+      console.log('createorderService API hit:', body.items[22].name)
       callback(undefined, {
 	      
 	  currency: body.base_currency_code
