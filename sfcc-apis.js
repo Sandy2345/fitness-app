b7398d3c7671b81f3f713a8ad3abd4ab4abf5fa7
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-24 15:19:54 +0530

    Update sfcc-apis.js

--- sfcc-apis.js
+++ sfcc-apis.js
@@ -73,7 +73,7 @@ var getProductDetailsService = (productName, callback) => {
        });
 };
 
-var getOrderService = (callback) => {
+var getOrderService = (city,applicationid, callback) => {
   console.log('hit order service api');
   request({
     url: `https://samples.openweathermap.org/data/2.5/weather?q=London&appid=c263e59bb171900b2d224854a55d06cf`,
