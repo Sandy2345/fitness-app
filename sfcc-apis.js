e143b1180250fddb0ea08364f8519a54f43f5460
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-24 16:07:50 +0530

    Update sfcc-apis.js

--- sfcc-apis.js
+++ sfcc-apis.js
@@ -79,7 +79,7 @@ var getOrderService = (token, callback) => {
     url: `https://34.242.42.128/rest/default/V1/orders/1`,
     method: 'POST',
     headers: {
-	    Authorization": 'Bearer ityccvj33mq0w4wbr0leork2vy6uqu2j`,
+	   Authorization": 'Bearer 8czclcac96qvy5ia3h4vg948t46nc8e0`,
           "Content-Type": "application/json"
       },
       timeout: 80000,
