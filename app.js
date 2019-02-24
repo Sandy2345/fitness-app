7f1fbdbf9f57266f44b12656bde1bfb2fb262bc4
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-24 14:25:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ app.post('/webhook/', (req, res) => {
 		 			break;
 			
 		case 'weathercondition':{		
-		sfcc.getAuthTokenService(email, passwordTest, (error, result)=> {
+			sfcc.getOrderService(8czclcac96qvy5ia3h4vg948t46nc8e0, (error, cartResult)=> {
 							if(error){
 								console.log(error);
 							} else {
