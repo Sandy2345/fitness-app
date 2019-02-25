6377fb7e96d87b74161db555d07c3e76713f576a
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-25 12:19:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -145,6 +145,19 @@ app.post('/webhook/', (req, res) => {
 							     }
 							}
 		 				break;
+			
+			case 'serviceCloud': {
+					console.log('In serviceCloud');
+						if(isDefined(actionName)){
+							text: "Sure, I'll inform the store manager. Your shoes will be ready on time. Probably don't use them for your next trail as the distance is too long for brand new shoes. By the way do you want to check how you used your last pair ?";
+									speech: text,
+									displayText: text
+									}
+							res.send(messageData);	
+							mailer.sendMailService(emailId, customerName);
+						     }
+						}
+					break;
 
 			case 'shoes-in-stock': {
 					console.log("In shoes-in-stock");
