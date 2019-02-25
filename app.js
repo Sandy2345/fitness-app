e0a6825546c7d67e49e9f3355fa93744e8cbd06e
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-25 10:48:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -513,7 +513,7 @@ app.post('/webhook/', (req, res) => {
 									} else {
 										var currency= cartResult.currency;
 										console.log(currency +"  "+cartResult.currency);
-										text='Yes, there is a $currency currently a promotion - they are at 200 swiss francs unt one';
+										text='Yes, there is a  +"  "+ cartResult.currency + currently a promotion - they are at 200 swiss francs unt one';
 										messageData = {
 												speech: text,
 												displayText: text
