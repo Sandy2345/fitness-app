79b8ecd0afdb3b1c4fe2c9ca910450698c494609
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-26 23:24:17 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -75,7 +75,7 @@ console.log('Create order api');
       callback(undefined, {
 	      
 	    name: body.items[0].name
-	    name : json.items.length
+	   // name : json.items.length
 	       //name : json.items.name.length
 	      //name: jsonData.items.length
         });
