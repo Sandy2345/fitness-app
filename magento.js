378b4a2a723bfed57837b1fa40404a68c20fdf24
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-28 12:34:05 +0530

    Update magento.js

--- magento.js
+++ magento.js
@@ -2,10 +2,10 @@ var ClientOAuth2 = require('client-oauth2')
 var sendAuth2 = (emailAddress, name)=> {
 var githubAuth = new ClientOAuth2({
   clientId: '5ffe4a99-49d6-47a5-857a-1df7ce25f92a',
-  //clientSecret: '123',
+  clientSecret: '123',
   accessTokenUri: 'https://login.microsoftonline.com/common/oauth2/authorize',
   authorizationUri: 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://adc-cg-poc.crm4.dynamics.com/'
-  callback: 'http://localhost',
+  redirect_uri: 'http://localhost/callback',
     scopes: ['notifications', 'gist'],
   //rejectUnauthorized: false
   
