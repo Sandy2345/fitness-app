65fef2717e39a7628d7709c8d3df1dd67896ed5f
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-28 12:14:47 +0530

    Update magento.js

--- magento.js
+++ magento.js
@@ -4,7 +4,7 @@ var githubAuth = new ClientOAuth2({
   clientId: '5ffe4a99-49d6-47a5-857a-1df7ce25f92a',
   //clientSecret: '123',
   accessTokenUri: 'https://login.microsoftonline.com/common/oauth2/authorize',
-  authorizationUri: 'https://login.microsoftonline.com/common/oauth2/authorize?resource={https://adc-cg-poc.crm4.dynamics.com/'
+  authorizationUri: 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://adc-cg-poc.crm4.dynamics.com/'
   //redirectUri: 'http://localhost',
  // scopes: ['notifications', 'gist'],
   //rejectUnauthorized: false
