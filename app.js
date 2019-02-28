49d491f6b6c5aac09812480440fc12b07c5589bf
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-28 12:05:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,9 +61,9 @@ const apiAiService = apiai(config.API_AI_CLIENT_ACCESS_TOKEN, {
 const sessionIds = new Map();
 
 // Index route
-app.get('Auth2/', function (req, res) {
+app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
-	magentoAuth.sendAuth2(error, finalResult);
+	//magentoAuth.sendAuth2(error, finalResult);
 	//console.log('sandeep')
 })
 
