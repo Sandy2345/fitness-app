c2d0b76fce53f2c419893cc938bb6aa7ff7b158c
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-02 16:04:53 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -47,6 +47,46 @@ var getAuthTokenService = (username, password, callback) =>{
 };
 
 
+
+var createorder = (authToken, callback) => {
+//var count = Object.keys(body).length;
+console.log('Create order api');
+  request({
+    url: `https://34.242.42.128/rest/default/V1/orders/1`,
+    method: 'GET',
+    timeout: 40000,
+    headers: {
+        "content-type": "application/json",
+        "authorization": `Bearer ${authToken}`
+      },
+    rejectUnauthorized: false,
+    json: true
+  }, (error, response, body) => {
+
+    if(error){
+      callback('There was an error connecting to the server');
+    }
+    else if(response.statusCode == 400){
+      console.log('Cart already present');
+      //console.log(body.base_currency_code);
+      callback(undefined, {
+       // basketId: body.fault.arguments.basketIds
+        });
+    }
+    else if(response.statusCode == 200){
+      	  //var jsonData = JSON.parse(body);
+	  //var namelengh = jsonData.items.length;   
+      console.log('createorderService API hit:',body.items[1].item_id)
+      callback(undefined, {
+	      orderNumber :body.items[1].item_id,
+	      name: body.items[0].name,
+	      name1:body.items[1].name
+        });
+      }
+    });
+
+};
+
 var getupdatedweather = (city,applicationid, callback) => {
 
         console.log('Update payment API hit');
