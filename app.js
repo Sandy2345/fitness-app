fc31bf0ed5e6820e9d7204bb494cd4b5de7ee767
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-03 09:59:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ app.post('/webhook/', (req, res) => {
 		 			break;
 			
 		case 'weathercondition':{		
-		magento.getupdatedweather('city', 'appid', (error, result)=> {
+		magento.getAuthTokenService(email, password, (error, result)=> {
 							if(error){
 								console.log(error);
 							} else {
