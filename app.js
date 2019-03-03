da2e70b20d914a1970b24537d9d0440e3d9c76d8
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-03 10:55:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -202,8 +202,8 @@ app.post('/webhook/', (req, res) => {
 								//setTimeout(() => pushNotification(deviceIdJ), 3000);
 								text="I am sending you the options, please check on your app.";
 								messageData = {
- 										speech:text,
- 										displayText:text
+ 										speech: text,
+ 										displayText: text
  										}
  								res.send(messageData);	
  								}
