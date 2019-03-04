75a73d8b0ddae7aa3978184d3ee83dc822cdb7f8
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 15:22:56 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -127,36 +127,6 @@ request.end();
 
 };
 
-var getdynamicValue = (authToken, callback) =>{
-//var count = Object.keys(body).length;
-console.log('Create order api');
-  request({
-    url: `https://adc-cg-poc.api.crm4.dynamics.com/api/data/v9.1/contacts?$select=lastname,'
-    method: 'GET',
-    timeout: 40000,
-    headers: {
-        "content-type": "application/json"
-      },
-    rejectUnauthorized: false,
-    json: true
-  }, (error, response, body) => {
-
-    if(error){
-      callback('There was an error connecting to the server');
-    }
-    else if(response.statusCode == 400){
-      console.log('Cart already present');
-    }
-    else if(response.statusCode == 200){ 
-      console.log('createorderService API hit:')
-      callback(undefined, {
-        });
-      }
-    });
-
-};
-
-
 function isEmpty(obj) {
     for(var key in obj) {
         if(obj.hasOwnProperty(key))
