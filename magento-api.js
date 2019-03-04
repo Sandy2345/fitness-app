8fc531eda4718316ade25ddd81035a85a80f8090
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 10:22:42 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -53,7 +53,7 @@ console.log('Create order api');
   request({
     url: `https://34.242.42.128/rest/default/V1/orders/1`,
     method: 'GET',
-    timeout: 40000,
+    //timeout: 40000,
     headers: {
         "content-type": "application/json",
         "authorization": `Bearer ${authToken}`
@@ -86,66 +86,6 @@ console.log('Create order api');
 
 };
 
-var dynamicAuthToken = (callback) => {
-//configuration details
-//mostly extracted from Azure 
-//--> app registered as native application in Azure AD
-const https = require('https');
-var crmorg = 'https://adc-cg-poc.crm4.dynamics.com';
-var username = 'Adobe2@capgeminidcxdemo.onmicrosoft.com';
-var userpassword = 'Adccrm@123';
-var authhost = 'login.microsoftonline.com';
-var authpath = 'https://login.microsoftonline.com/392474b0-b713-4e33-93f8-8be0836e11e3/oauth2/token';
-var clientid = '5ffe4a99-49d6-47a5-857a-1df7ce25f92a';
-
-//token request parameters
-var postData = 'client_id=' + clientid;
-postData += '&resource=' + encodeURIComponent(crmorg);
-postData += '&username=' + encodeURIComponent(username);
-postData += '&password=' + encodeURIComponent(userpassword);
-postData += '&grant_type=password';
-
-//set the token request parameters
-var options = {
-    host: authhost,
-    path: authpath,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
-    }
-};
-
-//make the token request
-var request = https.request(options, (response) => {
-    let data = '';
-
-    //  A chunk of data has been recieved
-    response.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    // The whole response has been recieved
-    response.on('end', () => {
-	    console.log('sandeeeeeeeeeeeepppppppp');
-	    console.log(data);
-	    console.log('priyaaaaaaaaaaaaaaaaa');
-        var tokenresponse = JSON.parse(data);
-        var access_token = tokenresponse.access_token;
-        console.log('testing token priya : ' + access_token);
-    });
-});
-
-request.on('error', (e) => {
-    console.error(e);
-});
-
-request.write(postData);
-request.end();
-
-};
-
-
 function isEmpty(obj) {
     for(var key in obj) {
         if(obj.hasOwnProperty(key))
@@ -156,7 +96,6 @@ function isEmpty(obj) {
 
 module.exports = {
     getAuthTokenService,
-    createorder,
-    dynamicAuthToken
+    createorder
     
 };
