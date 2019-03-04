8b96d7b2134e379e7bd61a9875f70c6a8bd4418d
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 19:18:04 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -151,7 +151,7 @@ console.log('Create dynamic api');
 	  var Parseresponse = JSON.stringify(body);
         callback(undefined, {
 		//body: Parseresponse
-		name : Parseresponse.value
+		name : Parseresponse.lastname
         });
       }
     });
