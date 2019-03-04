25de095c3932c2c3f9cac5454803d340468eb737
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 16:52:53 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -148,9 +148,7 @@ console.log('Create dynamic api');
       console.log('400 in dynamic api');
     }
     else if(response.statusCode == 200){ 
-	console.log('createorderService API hit:');
-      callback(undefined, {
-	      //body : body
+        callback(undefined, {
         });
       }
     });
