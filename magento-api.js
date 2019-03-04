54c281bcebc325a353c909b9125af58a57a3670e
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 16:42:26 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -130,7 +130,7 @@ request.end();
 var getdynamic = (authToken,lastname, callback) =>{
 console.log('Create dynamic api');
   request({
-    url: 'https://adc-cg-poc.api.crm4.dynamics.com/api/data/v9.1/contacts?$select={lastname}' ,
+    url: 'https://adc-cg-poc.api.crm4.dynamics.com/api/data/v9.1/contacts' ,
     method: 'GET',
     timeout: 40000,
     headers: {
