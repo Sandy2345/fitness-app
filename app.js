bc0f09bc11da4a3496669cb618a792f4675eabe6
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 23:35:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var token = '';
 var text = '';
 var cardId;
 var basketId;
+var contactid;
 var payment_id;
 var customer_id;
 var emailId;
@@ -182,6 +183,7 @@ app.post('/webhook/', (req, res) => {
                                 } else {
 					 console.log('sandeep1');
 					console.log(cartResult.name);
+					var contactid = cartResult.name;
 					 console.log('sandeep2');
                                      //console.log('Code--->', result.code);
 					//console.log(cartResult.body);
@@ -194,6 +196,9 @@ app.post('/webhook/', (req, res) => {
                                     res.send(messageData);
                                 }
                             });
+			magento.updateDynamic(result.code, contactid, (error, result) => {
+				console.log('patch');
+		        });
                         }
                     });
                 }
