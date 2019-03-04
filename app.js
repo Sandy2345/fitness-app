ee303e5e706823589abaeee78820fa00aaeb5e8b
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 16:14:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -174,12 +174,12 @@ app.post('/webhook/', (req, res) => {
                         if (error) {
                             console.log(error);
                         } else {
-                            magento.getdynamic(result.code, lastname, (error, cartResult) => {
+                            magento.getdynamic(result.code, (error, cartResult) => {
 				     console.log('sandeep');
                                 if (error) {
                                     console.log(error);
                                 } else {
-                                    console.log('Code--->', result.code);
+                                   // console.log('Code--->', result.code);
                                     //console.log(result.token+' '+result.customer_id+" "+result.email);
                                     text = "Yes, there is currently a promotion - they are at 200 swiss francs until the end of the month and are available at your usual Cap Sports Style store. Same color as current one";
                                     messageData = {
