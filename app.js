5d3aeed80c4f4bb99add25dbc745f8716d50580d
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-04 18:26:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -181,9 +181,9 @@ app.post('/webhook/', (req, res) => {
                                     console.log(error);
                                 } else {
 					 console.log('sandeep');
-					console.log(name);
+					//console.log(name);
                                   // console.log('Code--->', result.code);
-					//console.log(cartResult.body);
+					console.log(cartResult.body);
                                     //console.log(result.token+' '+result.customer_id+" "+result.email);
                                     text = "Yes, there is currently a promotion - they are at 200 swiss francs until the end of the month and are available at your usual Cap Sports Style store. Same color as current one";
                                     messageData = {
