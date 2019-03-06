7b42765b7e605fbd1addbee0cd6f3a5a9a2d736a
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-06 15:32:11 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -65,6 +65,14 @@ console.log(authToken);
 	    console.log(body);
 	     console.log("body values");
            console.log(body[0].order_id);
+	    for(var i =0; i < 4;i++){
+            var str = ""
+             str + ="You have 4 orders in your order list, and the details are " body[i].name + "it will be delivered at your shipping address in" + body[i].delivery_days + "days"
+             }
+	  callback(undefined, {
+		  console.log(str);
+	     ordernumber: body.items[1].item_id
+        });
      
       }
     });
