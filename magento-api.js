2191c8fd751ba74e1e1fc38c9499fd1634bf5920
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-06 16:55:57 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -62,17 +62,18 @@ console.log(authToken);
     }
     else if(response.statusCode == 200){ 
       console.log('createorderService API hit:',response.statusCode)
-      console.log(body);
-      console.log("body values");
-      console.log(body[0].order_id);
-      var str1 = "You have" + body[0].total_item_count + "orders in your order list, and the details are";
+     var str1 = "You have" + body[0].total_item_count + "orders in your order list, and the details are";
       var str2 = "";
       var str3 = "Is there anything else that I can help you with?"
       body.forEach(function(element) {
 	  str2 = str2 + element.name + "it will be delivered at your shipping address in" + element.delivery_days + "days";
       });
      var s= str1+str2+str3;
-     console.log(s);
+     console.log(s)
+     callback(undefined, {
+	     //ordernumber: body.items[1].item_id
+	    console.log(s) 
+        });
       }
     });
 
