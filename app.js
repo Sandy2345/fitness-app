88d93929308b9a543fbc2d9bb59df84842b7b367
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-10 22:16:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -187,8 +187,8 @@ app.post('/webhook/', (req, res) => {
 								console.log(error);
 							} else {
 						          console.log(JSON.stringify(req.body));
-						              var token=req.body.originalRequest.data.user.idToken;
-						              console.log(token);
+						             // var token=req.body.originalRequest.data.user.idToken;
+						             // console.log(token);
 								//console.log(JSON.stringify(decoded));
 								//console.log(decoded);
 								
@@ -204,41 +204,6 @@ app.post('/webhook/', (req, res) => {
 						   	});
 						}	
 		 break;
-
-		    case 'shoes-in-stockkk': {
-					console.log("In shoes-in-stock");
-					if(isDefined(actionName)){
-						//var idtoken=req.body.originalRequest.data.user.idToken;
-						//var decoded = jwtdecode(idtoken);
-						//console.log(decoded);
-					magento.getAuthTokenService((error, result)=> {
-							if(error){
-								console.log(error);
-							} else {
-								//customer_id=result.customer_id
-								//token=result.token
-								//emailId=result.email
-								//customerName=result.first_name
-								//custLastName=result.last_name
-								magento.createorder(result.code, (error, cartResult)=> {
-									if(error){
-										console.log(error);
-									} else {
-										//basketId=cartResult.basketId;
-										//console.log(result.token+' '+result.customer_id+" "+result.email);
-										text="Yes, there is currently a promotion - they are at 200 swiss francs until the end of the month and are available at your usual Cap Sports Style store. Same color as current one";
-										messageData = {
-												speech: text,
-												displayText: text
-												}
-										res.send(messageData);		
-								 	      }
-									});
-							     	}
-						   	});
- 						}
-					}
-		 			break;
 		    
 		    
                                        case 'input.welcome':{
@@ -294,9 +259,9 @@ app.post('/webhook/', (req, res) => {
 						if (error) {
 							console.log(error);
 						} else {
-							console.log('sandeep')
-							console.log(contactid);
-							console.log(cartResult.body);
+							//console.log('sandeep')
+							//console.log(contactid);
+							//console.log(cartResult.body);
 							var contactid = cartResult.name;
 							text = "Yes, there is currently a promotion - they are at 200 swiss francs until the end of the month and are available at your usual Cap Sports Style store. Same color as current one";
 							messageData = {
@@ -304,7 +269,7 @@ app.post('/webhook/', (req, res) => {
 								displayText: text
 							}
 							res.send(messageData);
-							console.log('updateDynamic');
+							//console.log('updateDynamic');
 							//console.log(contactid);
 							//console.log(result.code);
 							magento.updateDynamic(result.code, cartResult.name, (error, cartResultp) => {	
@@ -323,186 +288,7 @@ app.post('/webhook/', (req, res) => {
             }
             break;
 		    
-		     case 'dynamicValueno': {
-                  console.log("In shoes-in-stock");
-                   if (isDefined(actionName)) {
-                    magento.dynamicAuthToken((error, result) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            magento.getdynamic(result.code, (error, cartResult) => {
-						// console.log(result.code);
-						if (error) {
-							console.log(error);
-						} else {
-							console.log('sandeep')
-							console.log(contactid);
-							console.log(cartResult.body);
-							var contactid = cartResult.name;
-							text = "Yes, e color as current one";
-							messageData = {
-								speech: text,
-								displayText: text
-							}
-							res.send(messageData);
-							console.log('updateDynamic');
-							//console.log(contactid);
-							//console.log(result.code);
-							magento.updateDynamic2(result.code, cartResult.name, (error, cartResultp) => {	
-								console.log('shivavavvav');
-								console.log(result.code);
-								//var contactid = cartResult.name;
-								console.log(cartResult.name);
-								console.log('trtetteteteetet');
-							});
-						}
- 					
-					});
-				}
-			});
-                }
-            }
-            break;
-
-
-        case 'shoes-in-stock-order':
-            {
-                console.log('In shoes-in-stock-order');
-                console.log(basketId + "  " + token);
-                mailer.sendMailService(emailId, customerName, custLastName);
-                if (isDefined(actionName)) {
-                    var productName = req.body.result.contexts[0].parameters.sportsProducts
-                    if (productName == 'Gloves') {
-                        var product_id = '0001TG250001';
-                        messageId = 'MTY0OjExNDow';
-                        sfcc.addProductsToCart(token, product_id, basketId, (error, result) => {
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                console.log(result.responseCode);
-                                notify(emailId, messageId);
-                                //setTimeout(() => pushNotification(deviceIdJ), 3000);
-                                text = "I am sending you the options, please check on your app.";
-                                messageData = {
-                                    speech: text,
-                                    displayText: text
-                                }
-                                res.send(messageData);
-                            }
-                        });
-
-                        sfmc.getAuthTokenService((error, result) => {
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                deviceAccessToken = result.accessToken;
-                                console.log(result.accessToken);
-                            }
-                        });
-                    } else if (productName == 'Jackets') {
-                        var product_id = '883360541099';
-                        messageId = 'MTgwOjExNDow';
-                        sfcc.addProductsToCart(token, product_id, basketId, (error, result) => {
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                console.log(result.responseCode);
-                                notify(emailId, messageId);
-                                //setTimeout(() => pushNotification(), 3000);
-                                text = "I am sending you the options, please check on your app.";
-                                messageData = {
-                                    speech: text,
-                                    displayText: text
-                                }
-                                res.send(messageData);
-                            }
-                        });
-
-                        sfmc.getAuthTokenService((error, result) => {
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                deviceAccessToken = result.accessToken;
-                                console.log(result.accessToken);
-                            }
-                        });
-                    }
-                    //mailer.sendMailService(emailId, customerName);
-                }
-            }
-            break;
-
-        case 'check_color':
-            {
-                console.log("In check_color");
-                if (isDefined(actionName)) {
-                    sfcc.getAddressService(token, customer_id, (error, addressResult) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            customer_address_id = addressResult.customer_address_id;
-                            sfcc.setShipmentService(token, customer_address_id, basketId, (error, result) => {
-                                if (error) {
-                                    console.log(error);
-                                } else {
-                                    console.log(result.responseCode);
-                                    text = "I think this color is the best one to fit with your shoes and pant. You will look awesome with them.";
-                                    messageData = {
-                                        speech: text,
-                                        displayText: text
-                                    }
-                                    res.send(messageData);
-                                }
-                            });
-
-                        }
-                    });
-                }
-            }
-            break;
-
-        case 'color-confirmed':
-            {
-                console.log("In color-confirmed");
-                if (isDefined(actionName)) {
-                    sfcc.setShipmentIdService(token, basketId, (error, result) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            orderTotal = result.product_total;
-                            text = "I assume I need express delivery so you have it for your race. Do you need something else?";
-                            messageData = {
-                                speech: text,
-                                displayText: text
-                            }
-                            res.send(messageData);
-                        }
-                    });
-                }
-            }
-            break;
-
-
-        case 'process-order':
-            {
-                console.log("In process-order");
-                if (isDefined(actionName)) {
-                    sfcc.addPaymentService(token, basketId, customerName, orderTotal, (error, result) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            console.log(result.responseCode);
-                            text = "Can I use your saved card or Google pay ?";
-                            messageData = {
-                                speech: text,
-                                displayText: text
-                            }
-                            res.send(messageData);
-                        }
-                    });
-                }
-            }
-            break;
+		   
 
           case 'order_status':
             {
@@ -526,40 +312,6 @@ app.post('/webhook/', (req, res) => {
                                     res.send(messageData);
                                 }
                             });
-                        }
-                    });
-                }
-            }
-            break;
-            
-        case 'orderConfirmed':
-            {
-                console.log("In orderConfirmed");
-                if (isDefined(actionName)) {
-                    function myFunc(token, payment_id, order_no) {
-                        //console.log(`In updating payment method ${token} ${payment_id} ${order_no}`);
-                        sfcc.updatePaymentService(token, order_no, payment_id, orderTotal, (error, result) => {
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                console.log(result);
-                            }
-                        });
-                    };
-                    sfcc.placeOrderService(token, basketId, (error, result) => {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            payment_id = result.payment_id;
-                            orderCode = result.code;
-                            //console.log(result.code+"  "+result.payment_id);
-                            text = "Your order has been confirmed. They will be delivered to your home address before Saturday. Your store manager will wait for you on Friday to pick up your shoes.";
-                            messageData = {
-                                speech: text,
-                                displayText: text
-                            }
-                            res.send(messageData);
-                            setTimeout(() => myFunc(token, result.payment_id, result.code), 3000);
                         }
                     });
                 }
