4c04babe1aa502b16c740a18932539a3cf6d28e2
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-11 19:14:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -255,7 +255,8 @@ app.post('/webhook/', (req, res) => {
 						if(isDefined(actionName)){
 							console.log('ssssssssssss');
 							console.log(cartResult.body)
-						       text="I am sending you the options, please check on your app "  + cartResult.body;
+							console.log(cartResult.page)
+						       text="I am sending you the options, please check on your app "  + cartResult.body +  " "+ cartResult.page ;
 							messageData = {
 									speech: text,
 									displayText: text
