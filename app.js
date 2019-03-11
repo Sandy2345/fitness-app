2db91aa105c781cced8d73634447ba0937799a9b
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-11 18:43:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -252,6 +252,7 @@ app.post('/webhook/', (req, res) => {
 		             case 'serviceCloud': {
 					console.log('In serviceCloud');
 						if(isDefined(actionName)){
+							console.log('ssssssssssss');
 							text: "Sure, I'll inform the store manager. Your shoes will be ready on time. Probably don't use them for your next trail as the distance is too long for brand new shoes. By the way do you want to check how you used your last pair ?";
 							messageData = {
 									speech: text,
