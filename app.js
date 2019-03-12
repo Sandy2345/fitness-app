c622ba3c4150f5fdd3b65f5557cef2cdea8a8b30
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-12 12:20:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -310,6 +310,26 @@ app.post('/webhook/', (req, res) => {
                 });
             }
             break;
+             case 'lastweek':
+            {
+                magento.getvaluelastweek((error, cartResult) => {
+                    console.log('lastweek');
+                    if (isDefined(actionName)) {
+                        console.log('lastweek');
+                        console.log(cartResult.body)
+                        console.log(cartResult.page)
+                        text = "lastweek " + cartResult.body + " " + cartResult.page;
+                        messageData = {
+                            speech: text,
+                            displayText: text
+                        }
+                        res.send(messageData);
+                        //mailer.sendMailService(emailId, customerName);
+                    }
+                });
+            }
+            break;
+
 
      
         case 'dynamicValue':
