3ef31aada3cc14b0c7672709c3fe09a3063595b0
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-12 11:28:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -324,6 +324,25 @@ app.post('/webhook/', (req, res) => {
 						}
 					break;
 		    
+		      case 'month': {
+				      magento.getvaluemonth((error, cartResult)=> {
+					console.log('In serviceCloud');
+						if(isDefined(actionName)){
+							console.log('yesterday');
+							console.log(cartResult.body)
+							console.log(cartResult.page)
+						       text="month "  + cartResult.body +  " "+ cartResult.page ;
+							messageData = {
+									speech: text,
+									displayText: text
+									}
+							res.send(messageData);	
+							//mailer.sendMailService(emailId, customerName);
+						     }
+				      });
+						}
+					break;
+		    
 		    
 		    case 'dynamicValue': {
                   console.log("In shoes-in-stock");
