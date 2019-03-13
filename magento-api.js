6b5f9c431823e0da22b9da5f378b002a9d8efc05
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-03-13 10:22:14 +0530

    Update magento-api.js

--- magento-api.js
+++ magento-api.js
@@ -435,6 +435,83 @@ var getvaluelastmonth = (callback) => {
 
 
 };
+var getvalueyear = (callback) => {
+    var OmnitureAPI = require('node-omniture-api')
+    var omniture = new OmnitureAPI('payal.daryani@capgemini.com:Capgeminisandbox', 'e5eccca081d2a1a329ee56e41e451811');
+    var pageViews;
+    var dateFrom = new Date();
+    var dateTo = new Date();
+    dateFrom.setMonth(0);
+    dateFrom.setDate(01);
+    requestData = {
+        "reportDescription": {
+            "reportSuiteID": "geo1xxlon-we-can-mart",
+            "dateFrom": dateFrom.toISOString().slice(0, 10),
+            "dateTo": dateTo.toISOString().slice(0, 10),
+            "metrics": "[{ id: 'pageviews' }]"
+
+        }
+    }
+    console.log('inside value');
+    omniture.queueAndFetchReport(requestData, function(success, data) {
+        if (success) {
+
+            pageViews = data.report.totals[0];
+            console.log("sssssandceep");
+            console.log(data.report.totals[0]);
+            console.log("dgdggdgdgdgd");
+            console.log(pageViews);
+            callback(undefined, {
+                body: 'chirag',
+                page: data.report.totals[0]
+
+            });
+
+        } else {
+            pageViews = data;
+            console.error(data);
+        }
+    });
+	
+var getvaluelastyear = (callback) => {
+    var OmnitureAPI = require('node-omniture-api')
+    var omniture = new OmnitureAPI('payal.daryani@capgemini.com:Capgeminisandbox', 'e5eccca081d2a1a329ee56e41e451811');
+    var pageViews;
+    var dateFrom = new Date();
+    var dateTo = new Date();
+    dateFrom.setYear(dateFrom.getFullYear() - 1);
+    requestData = {
+        "reportDescription": {
+            "reportSuiteID": "geo1xxlon-we-can-mart",
+            "dateFrom": dateFrom.toISOString().slice(0, 10),
+            "dateTo": dateTo.toISOString().slice(0, 10),
+            "metrics": "[{ id: 'pageviews' }]"
+
+        }
+    }
+    console.log('inside value');
+    omniture.queueAndFetchReport(requestData, function(success, data) {
+        if (success) {
+
+            pageViews = data.report.totals[0];
+            console.log("sssssandceep");
+            console.log(data.report.totals[0]);
+            console.log("dgdggdgdgdgd");
+            console.log(pageViews);
+            callback(undefined, {
+                body: 'chirag',
+                page: data.report.totals[0]
+
+            });
+
+        } else {
+            pageViews = data;
+            console.error(data);
+        }
+    });
+
+
+
 
 
 
@@ -551,5 +628,7 @@ module.exports = {
     getvalueweek,
     getvaluelastweek,
     getvaluemonth,
-    getvaluelastmonth
+    getvaluelastmonth,
+	getvalueyear,
+	getvaluelastyear
 };
